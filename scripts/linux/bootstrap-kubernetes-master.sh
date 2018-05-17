35a4f239eaa7572842de8c48b859b498cdf6fe38
Marco Ferrari <ferrari.marco@gmail.com>
2018-05-17 09:30:38 +0200

    Initialize helm

--- scripts/linux/bootstrap-kubernetes-master.sh
+++ scripts/linux/bootstrap-kubernetes-master.sh
@@ -18,3 +18,5 @@ chown vagrant:vagrant /home/vagrant/.kube/config
 
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+
+helm init
