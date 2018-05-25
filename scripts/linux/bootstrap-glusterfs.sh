03b9bc4ebe0a8b8e58804bb6650fcd761052d904
Marco Ferrari <ferrari.marco@gmail.com>
2018-05-25 10:38:16 +0200

    Delete the gluster archive file after unzipping

--- scripts/linux/bootstrap-glusterfs.sh
+++ scripts/linux/bootstrap-glusterfs.sh
@@ -10,6 +10,7 @@ mv master.zip "$gluster_kubernetes_archive_path"
 gluster_kubernetes_destination_path="/opt/gluster-kubernetes"
 mkdir -p "$gluster_kubernetes_destination_path"
 unzip "$gluster_kubernetes_archive_path" -d "$gluster_kubernetes_destination_path"
+rm "$gluster_kubernetes_archive_path"
 "$gluster_kubernetes_destination_path"/gluster-kubernetes-master/deploy/gk-deploy -gvy /vagrant/kubernetes/glusterfs/topology.json
 
 kubectl apply -f /vagrant/kubernetes/glusterfs/storage-class.yaml
