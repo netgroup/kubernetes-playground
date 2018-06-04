4f3f455ee6f579e3146c946051d6b5631a88b762
Marco Ferrari <ferrari.marco@gmail.com>
2018-06-04 15:15:09 +0200

    Workaround for internal DNS resolution on kube-controller-manager

--- scripts/linux/bootstrap-glusterfs.sh
+++ scripts/linux/bootstrap-glusterfs.sh
@@ -5,4 +5,9 @@ set -e
 gluster_kubernetes_destination_path="/opt"
 "$gluster_kubernetes_destination_path"/gluster-kubernetes-master/deploy/gk-deploy -gvy /vagrant/kubernetes/glusterfs/topology.json
 
-kubectl apply -f /vagrant/kubernetes/glusterfs/storage-class.yaml
+
+# Workaround for https://github.com/kubernetes/kubernetes/issues/42306
+heketi_service_ip="$(kubectl get svc heketi -o=jsonpath='{.spec.clusterIP}')"
+sed "s/heketi.default.svc.cluster.local/$heketi_service_ip/g" /vagrant/kubernetes/glusterfs/storage-class.yaml > /vagrant/kubernetes/glusterfs/storage-class.yaml.local
+
+kubectl apply -f /vagrant/kubernetes/glusterfs/storage-class.yaml.local
