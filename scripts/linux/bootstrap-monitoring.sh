543ce51dea4a2e10b5203e086ac73f59afe2df87
Marco Ferrari <ferrari.marco@gmail.com>
2018-06-04 16:34:16 +0200

    Fix helm path in bootstrap-monitoring.sh

--- scripts/linux/bootstrap-monitoring.sh
+++ scripts/linux/bootstrap-monitoring.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-helm install stable/prometheus --name prometheus --namespace prometheus --values /vagrant/kubernetes/prometheus/values.yaml
-helm install stable/grafana --name grafana --namespace grafana --values /vagrant/kubernetes/grafana/values.yaml
+/usr/local/bin/helm install stable/prometheus --name prometheus --namespace prometheus --values /vagrant/kubernetes/prometheus/values.yaml
+/usr/local/bin/helm install stable/grafana --name grafana --namespace grafana --values /vagrant/kubernetes/grafana/values.yaml
