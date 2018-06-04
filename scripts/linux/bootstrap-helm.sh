0a792e320f5246b020193312a6b75f9a5e51f659
Marco Ferrari <ferrari.marco@gmail.com>
2018-06-04 12:24:16 +0200

    Fix bootstrap-helm.sh script

--- scripts/linux/bootstrap-helm.sh
+++ scripts/linux/bootstrap-helm.sh
@@ -3,12 +3,12 @@
 set -e
 
 kubectl create -f /vagrant/kubernetes/tiller/rbac.yaml
-helm init --service-account tiller
+/usr/local/bin/helm init --service-account tiller
 
 WAIT=60
 s=0
 tiller_service=""
-debug -n "Determining tiller service URL ... "
+echo "Determining tiller service URL ... "
 while [[ "x${tiller_service}" == "x" ]] || [[ "${tiller_service}" == "<none>" ]]; do
   if [[ ${s} -ge ${WAIT} ]]; then
     echo "Timed out waiting for tiller service."
@@ -17,4 +17,5 @@ while [[ "x${tiller_service}" == "x" ]] || [[ "${tiller_service}" == "<none>" ]]
   sleep 10
   ((s+=1))
   tiller_service=$(kubectl describe svc tiller-deploy --namespace kube-system | grep "Endpoints:" | awk '{print $2}')
+  echo "Tiller service URL: $tiller_service"
 done
