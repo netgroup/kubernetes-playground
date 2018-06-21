3eb4b3f733b294026d43e45c4a6d144ded8a9298
Marco Ferrari <ferrari.marco@gmail.com>
2018-06-21 16:45:02 +0200

    Generate TLS certificate with Ansible crypto

--- scripts/linux/bootstrap-ingress-controller.sh
+++ scripts/linux/bootstrap-ingress-controller.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-openssl req -newkey rsa:4096 -nodes -keyout tls.key -x509 -days 365 -out tls.crt -subj "/C=IT/ST=Lazio/L=Rome /O=Dedalus/OU=Hero/CN=*.kubernetes-playground.local/emailAddress=marco.ferrari@dedalus.eu"
-kubectl create secret generic traefik-cert --from-file=tls.crt --from-file=tls.key --namespace kube-system
+kubectl create secret generic traefik-cert --from-file=/opt/tls/self_signed/tls.crt --from-file=/opt/tls/self_signed/tls.key --namespace kube-system
 
 kubectl apply -f /vagrant/kubernetes/traefik
--- scripts/linux/install-kubernetes.sh
+++ scripts/linux/install-kubernetes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v /vagrant/ansible:/etc/ansible --net=host ferrarimarco/open-development-environment-ansible:2.5.1-alpine /bin/sh -c "ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook /etc/ansible/playbooks/kubernetes.yml"
+docker run --rm -v /vagrant/ansible:/etc/ansible --net=host ferrarimarco/open-development-environment-ansible:2.5.1-alpine /bin/sh -c "apk add --no-cache --update openssl && pip install pyopenssl && ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook /etc/ansible/playbooks/kubernetes.yml"
