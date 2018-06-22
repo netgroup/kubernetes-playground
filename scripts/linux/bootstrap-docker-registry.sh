03ed6724f6e46c6f5942550c997a2aa056e83755
Marco Ferrari <ferrari.marco@gmail.com>
2018-06-22 14:35:41 +0200

    Store TLS crts in dedicated secrets

--- scripts/linux/bootstrap-docker-registry.sh
+++ scripts/linux/bootstrap-docker-registry.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-/usr/local/bin/helm install stable/docker-registry --name docker-registry --namespace docker-registry --values /vagrant/kubernetes/docker-registry/values.yaml
+kubernetes_namespace="docker-registry"
+
+kubectl create secret generic docker-registry-tls-cert --from-file=/opt/tls/self_signed/tls.crt --from-file=/opt/tls/self_signed/tls.key --namespace "$kubernetes_namespace"
+
+/usr/local/bin/helm install stable/docker-registry --name docker-registry --namespace "$kubernetes_namespace" --values /vagrant/kubernetes/docker-registry/values.yaml
--- scripts/linux/bootstrap-ingress-controller.sh
+++ scripts/linux/bootstrap-ingress-controller.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-kubectl create secret generic traefik-cert --from-file=/opt/tls/self_signed/tls.crt --from-file=/opt/tls/self_signed/tls.key --namespace kube-system
+kubectl create secret generic traefik-ui-tls-cert --from-file=/opt/tls/self_signed/tls.crt --from-file=/opt/tls/self_signed/tls.key --namespace kube-system
 
 kubectl apply -f /vagrant/kubernetes/traefik
