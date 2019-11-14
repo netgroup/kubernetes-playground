ac084a23486d5bce8b9e48472b10a1441655560d
Marco Ferrari <ferrari.marco@gmail.com>
2019-11-14 12:15:10 +0100

    Add a script to bootstrap a Debian installation as a dev environment

---
+++ scripts/linux/setup-debian.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list.d/virtualbox.list
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+
+apt-get update
+apt-get install virtualbox-6.0
