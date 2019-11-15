43de7e613b4ebc41dfbaf2083bf93d0ddf2b334a
Marco Ferrari <ferrari.marco@gmail.com>
2019-11-15 10:58:20 +0100

    Don't install openssl in the ansible container

--- scripts/linux/install-kubernetes.sh
+++ scripts/linux/install-kubernetes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v /vagrant/ansible:/etc/ansible -v /vagrant/ansible/playbooks/files/tls:/opt/tls/self_signed --net=host ferrarimarco/open-development-environment-ansible:2.5.5-alpine /bin/sh -c "apk add --no-cache --update openssl && pip install pyopenssl && ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook /etc/ansible/playbooks/kubernetes.yml && ansible-playbook /etc/ansible/playbooks/openssl-self-signed-certificate.yml"
+docker run --rm -v /vagrant/ansible:/etc/ansible -v /vagrant/ansible/playbooks/files/tls:/opt/tls/self_signed --net=host ferrarimarco/open-development-environment-ansible:2.5.5-alpine /bin/sh -c "ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook /etc/ansible/playbooks/kubernetes.yml && ansible-playbook /etc/ansible/playbooks/openssl-self-signed-certificate.yml"
