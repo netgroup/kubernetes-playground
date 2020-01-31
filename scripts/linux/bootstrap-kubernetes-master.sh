7d1a3d93c3b84b26285cb0847daa77e32bc0cb19
Marco Ferrari <ferrari.marco@gmail.com>
2020-02-01 00:53:55 +0100

    Fix shellcheck issues

--- scripts/linux/bootstrap-kubernetes-master.sh
+++ scripts/linux/bootstrap-kubernetes-master.sh
@@ -8,7 +8,7 @@ kubeadm init --config "$configuration_file_path"
 # Setup root user environment
 mkdir -p "$HOME"/.kube
 cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
-chown $(id -u):$(id -g) "$HOME"/.kube/config
+chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Setup vagrant user environment
 mkdir -p /home/vagrant/.kube
@@ -18,9 +18,10 @@ chown vagrant:vagrant /home/vagrant/.kube/config
 network_plugin_id="$2"
 echo "Installing $network_plugin_id network plugin"
 
-if [[ "$network_plugin_id" == 'weavenet' ]]; then
-    export kubever=$(kubectl version | base64 | tr -d '\n')
+if [ "$network_plugin_id" == 'weavenet' ]; then
+    kubever=$(kubectl version | base64 | tr -d '\n')
+    export kubever
     kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
-elif [[ "$network_plugin_id" == 'calico' ]]; then
+elif [ "$network_plugin_id" == 'calico' ]; then
     kubectl apply -f https://docs.projectcalico.org/v3.10/manifests/calico.yaml
 fi
