10bee9686ff53f37499dcea358f17958235bef3e
Marco Ferrari <ferrari.marco@gmail.com>
2020-02-17 09:34:58 +0100

    Implement a two-steps provisioning process for cluster nodes (#35)
    Provision and configure the environment in two steps. Close #31

--- scripts/linux/install-kubernetes.sh
+++ scripts/linux/install-kubernetes.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-docker run --rm -v /vagrant/ansible:/etc/ansible -v /vagrant/ansible/playbooks/files/tls:/opt/tls/self_signed --net=host ferrarimarco/open-development-environment-ansible:2.7.12-alpine /bin/sh -c "ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook /etc/ansible/playbooks/kubernetes.yml && ansible-playbook /etc/ansible/playbooks/openssl-self-signed-certificate.yml"
+if ! TEMP="$(getopt -o vdm: --long inventory: -n 'install-kubernetes' -- "$@")" ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+
+inventory=
+
+while true; do
+  case "$1" in
+    -u | --inventory ) inventory="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+echo "Ensure the Docker service is enabled and running"
+systemctl enable docker
+systemctl restart docker
+
+inventory="/etc/"$inventory
+
+echo "Running Ansible playbooks against $inventory inventory"
+docker run --rm \
+    -v /vagrant/ansible:/etc/ansible \
+    -v /vagrant/ansible/playbooks/files/tls:/opt/tls/self_signed \
+    --net=host \
+    ferrarimarco/open-development-environment-ansible:2.7.12-alpine \
+    /bin/sh -c "ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook -i $inventory /etc/ansible/playbooks/kubernetes.yml && ansible-playbook -i $inventory /etc/ansible/playbooks/openssl-self-signed-certificate.yml"
