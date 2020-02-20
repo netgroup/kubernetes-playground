e6ff50b40b39846825defec334f51cbdc47ad48e
Stefano Salsano <stefano.salsano@uniroma2.it>
2020-02-20 15:54:29 +0100

    fix ipv6 setup script

--- scripts/linux/configure-ipv6-interface.sh
+++ scripts/linux/configure-ipv6-interface.sh
@@ -7,7 +7,8 @@ DEVICE="$(ip -o addr show scope global | grep "$CURRENT_IPV4_ADDRESS" | awk '{pr
 echo "Current IPv4 address $CURRENT_IPV4_ADDRESS assigned to $DEVICE"
 
 IPV6_ADDRESS="$1"
+IPV6_NETMASK="$3"
 echo "Adding $IPV6_ADDRESS to $DEVICE"
-ip -6 address add "$IPV6_ADDRESS" dev "$DEVICE"
+ip -6 address add "$IPV6_ADDRESS/$IPV6_NETMASK" dev "$DEVICE"
 
 echo "Getting all the IP addresses: $(ip -o addr show scope global)"
