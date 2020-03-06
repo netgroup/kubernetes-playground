784bcdc14b7a1c30d85486e2d17404ecb95b37d3
Stefano Salsano <stefano.salsano@uniroma2.it>
2020-03-07 00:07:03 +0100

    persistent ipv6 address

--- scripts/linux/configure-ipv6-interface.sh
+++ scripts/linux/configure-ipv6-interface.sh
@@ -6,9 +6,17 @@ CURRENT_IPV4_ADDRESS="$2"
 DEVICE="$(ip -o addr show scope global | grep "$CURRENT_IPV4_ADDRESS" | awk '{print $2}')"
 echo "Current IPv4 address $CURRENT_IPV4_ADDRESS assigned to $DEVICE"
 
+UUID="$(nmcli -g UUID,DEVICE con show | grep "$DEVICE" | awk -F ":" '{print $1}')"
+echo "Current NetworkManager UUID $UUID assigned to $DEVICE"
+
 IPV6_ADDRESS="$1"
 IPV6_NETMASK="$3"
 echo "Adding $IPV6_ADDRESS to $DEVICE"
-ip -6 address add "$IPV6_ADDRESS/$IPV6_NETMASK" dev "$DEVICE"
+#ip -6 address add "$IPV6_ADDRESS/$IPV6_NETMASK" dev "$DEVICE"
+nmcli con mod $UUID ipv6.address "$IPV6_ADDRESS/$IPV6_NETMASK" ipv6.method manual
+
+echo "Restarting NetworkManager"
+systemctl restart network
 
 echo "Getting all the IP addresses: $(ip -o addr show scope global)"
+
