75e0926d2304eeb94ad44a9ec91f189238577aaf
Stefano Salsano <stefano.salsano@uniroma2.it>
2020-03-07 13:12:29 +0100

    fixing install-kubernetes.sh

--- scripts/linux/install-kubernetes.sh
+++ scripts/linux/install-kubernetes.sh
@@ -15,18 +15,16 @@ done
 
 ansible_debug="$1"
 
-echo "Ansible debug: $ansible_debug"
+echo "Ansible debug flag: $ansible_debug"
 
 verbose_flag=
-if [ "$ansible_debug" = 'on' ]; then
+if [ "$ansible_debug" = "on" ]; then
     verbose_flag="-vv"
 fi
 
-echo "Verbose verbose_flag: $verbose_flag"
+echo "Verbose verbose_flag value: $verbose_flag"
 
 echo "Ensure the Docker service is enabled and running"
-
-exit
 
 systemctl enable docker
 systemctl restart docker
