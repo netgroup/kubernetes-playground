8657b88447004e2d900049a1b2c32d9780d7e355
Stefano Salsano <stefano.salsano@uniroma2.it>
2020-03-12 18:15:24 +0100

    Make the IPv6 address persistent (#65)
    Co-authored-by: Marco Ferrari <ferrari.marco@gmail.com>

--- scripts/linux/configure-ipv6-interface.sh
+++ scripts/linux/configure-ipv6-interface.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
+echo "making IPv4 forwarding permanent"
+
+grep -Fv net.ipv4.ip_forward /etc/sysctl.conf >/etc/sysctl.conf.mytmp
+echo "net.ipv4.ip_forward=1" >>/etc/sysctl.conf.mytmp
+mv /etc/sysctl.conf.mytmp /etc/sysctl.conf
+
 echo "Configuring the IPv6 interface"
 
 CURRENT_IPV4_ADDRESS="$2"
 DEVICE="$(ip -o addr show scope global | grep "$CURRENT_IPV4_ADDRESS" | awk '{print $2}')"
 echo "Current IPv4 address $CURRENT_IPV4_ADDRESS assigned to $DEVICE"
 
+UUID="$(nmcli -g UUID,DEVICE con show | grep "$DEVICE" | awk -F ":" '{print $1}')"
+echo "Current NetworkManager UUID $UUID assigned to $DEVICE"
+
 IPV6_ADDRESS="$1"
 IPV6_NETMASK="$3"
 echo "Adding $IPV6_ADDRESS to $DEVICE"
-ip -6 address add "$IPV6_ADDRESS/$IPV6_NETMASK" dev "$DEVICE"
+nmcli con mod "$UUID" ipv6.address "$IPV6_ADDRESS/$IPV6_NETMASK" ipv6.method manual
+
+echo "Restarting NetworkManager"
+systemctl restart network
 
 echo "Getting all the IP addresses: $(ip -o addr show scope global)"
