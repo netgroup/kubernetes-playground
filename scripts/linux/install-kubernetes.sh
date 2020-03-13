a0a0f051a82cb95045e49606a9d9452ad676471f
Marco Ferrari <ferrari.marco@gmail.com>
2020-03-13 17:24:09 +0100

    Use argument to parse additional ansible args

--- scripts/linux/install-kubernetes.sh
+++ scripts/linux/install-kubernetes.sh
@@ -1,44 +1,42 @@
 #!/bin/sh
 
-if ! TEMP="$(getopt -o vdm: --long inventory: -n 'install-kubernetes' -- "$@")" ; then echo "Terminating..." >&2 ; exit 1 ; fi
+if ! TEMP="$(getopt -o a:i: --long additional-ansible-arguments:,inventory: -n 'install-kubernetes' -- "$@")"; then
+    echo "Terminating..." >&2
+    exit 1
+fi
 eval set -- "$TEMP"
 
+additional_ansible_arguments=
 inventory=
 
 while true; do
-  case "$1" in
-    -u | --inventory ) inventory="$2"; shift 2 ;;
-    -- ) shift; break ;;
-    * ) break ;;
-  esac
+    case "$1" in
+    -a | --additional-ansible-arguments)
+        additional_ansible_arguments="$2"
+        shift 2
+        ;;
+    -i | --inventory)
+        inventory="$2"
+        shift 2
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *) break ;;
+    esac
 done
 
-verbose_output="$1"
-
-echo "verbose_output: $verbose_output"
-
-verbose_flag=
-if [ "$verbose_output" = "on" ]; then
-    verbose_flag="-vv"
-fi
-
-echo "verbose_flag: $verbose_flag"
-
 echo "Ensure the Docker service is enabled and running"
-
 systemctl enable docker
 systemctl restart docker
 
-inventory="/etc/"$inventory
-
-echo "Running Ansible playbooks against $inventory inventory"
-
-
+inventory="/etc/$inventory"
+echo "Running Ansible playbooks against $inventory inventory, with additional arguments: $additional_ansible_arguments"
 docker run --rm \
     -v /vagrant/ansible:/etc/ansible \
     -v /vagrant/ansible/playbooks/files/tls:/opt/tls/self_signed \
     --net=host \
     ferrarimarco/open-development-environment-ansible:2.7.12-alpine \
-    /bin/sh -c "ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook -i $inventory /etc/ansible/playbooks/kubernetes.yml $verbose_flag && ansible-playbook -i $inventory /etc/ansible/playbooks/openssl-self-signed-certificate.yml" \
+    /bin/sh -c "ansible-galaxy install -r /etc/ansible/requirements.yml && ansible-playbook -i $inventory $additional_ansible_arguments /etc/ansible/playbooks/kubernetes.yml && ansible-playbook -i $inventory $additional_ansible_arguments /etc/ansible/playbooks/openssl-self-signed-certificate.yml" \
     2>&1 | tee /vagrant/ansible_output.txt
-
