262d766894593cd7c445a65ba5f4e1ea260306d0
Marco Ferrari <ferrari.marco@gmail.com>
2020-03-18 17:30:05 +0100

    Move Helm values in the helm directory

--- scripts/linux/bootstrap-monitoring.sh
+++ scripts/linux/bootstrap-monitoring.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-/usr/local/bin/helm install stable/prometheus --name prometheus --namespace prometheus --values /vagrant/kubernetes/prometheus/values.yaml
-/usr/local/bin/helm install stable/grafana --name grafana --namespace grafana --values /vagrant/kubernetes/grafana/values.yaml
+/usr/local/bin/helm install stable/prometheus --name prometheus --namespace prometheus --values /vagrant/helm/prometheus/values.yaml
+/usr/local/bin/helm install stable/grafana --name grafana --namespace grafana --values /vagrant/helm/grafana/values.yaml
