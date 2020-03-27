98272e7d8a565fbfb79d144d41b42d54f92000f8
Stefano Salsano <stefano.salsano@uniroma2.it>
2020-03-27 13:05:32 +0100

    fix installation scripts

--- scripts/linux/ci/install-libvirt.sh
+++ scripts/linux/ci/install-libvirt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # installation script for libvirt
+# to be run as root
 # tested on debian 10
 
 apt-get install -y --no-install-recommends qemu-kvm=1:3.1+dfsg-8+deb10u4 \
-libvirt-clients=5.0.0-4+deb10u1 libvirt-daemon-system=5.0.0-4+deb10u1
+    libvirt-clients=5.0.0-4+deb10u1 libvirt-daemon-system=5.0.0-4+deb10u1
