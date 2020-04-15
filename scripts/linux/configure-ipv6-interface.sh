2b28490d371a8b7dc559dd29157cdaf2d95003e7
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-15 23:52:12 +0200

    Some more DRY

--- scripts/linux/configure-ipv6-interface.sh
+++ scripts/linux/configure-ipv6-interface.sh
@@ -2,15 +2,16 @@
 
 echo "Configuring the IPv6 interface"
 
+IPV6_ADDRESS="$1"
 CURRENT_IPV4_ADDRESS="$2"
-DEVICE="$(ip -o addr show scope global | grep "$CURRENT_IPV4_ADDRESS" | awk '{print $2}')"
+IPV6_NETMASK="$3"
+DEVICE="$4"
+UUID="$5"
+
 echo "Current IPv4 address $CURRENT_IPV4_ADDRESS assigned to $DEVICE"
 
-UUID="$(nmcli -g UUID,DEVICE con show | grep "$DEVICE" | awk -F ":" '{print $1}')"
 echo "Current NetworkManager UUID $UUID assigned to $DEVICE"
 
-IPV6_ADDRESS="$1"
-IPV6_NETMASK="$3"
 echo "Adding $IPV6_ADDRESS to $DEVICE"
 nmcli con mod "$UUID" ipv6.address "$IPV6_ADDRESS/$IPV6_NETMASK" ipv6.method manual
 
