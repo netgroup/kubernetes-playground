bb6026d9acb2776db86afd782420cd45a5d64143
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-17 00:51:43 +0200

    Install libvirt on travis

--- scripts/linux/ci/install-libvirt.sh
+++ scripts/linux/ci/install-libvirt.sh
@@ -4,6 +4,17 @@
 # tested on debian 10
 
 apt-get install -y --no-install-recommends \
+    dnsmasq-base=2.80-1 \
+    ebtables=2.0.10.4+snapshot20181205-3 \
+    libguestfs-tools=1:1.40.2-2 \
     libvirt-clients=5.0.0-4+deb10u1 \
     libvirt-daemon-system=5.0.0-4+deb10u1 \
-    qemu-kvm=1:3.1+dfsg-8+deb10u4
+    libvirt-dev=5.0.0-4+deb10u1 \
+    libxml2-dev=2.9.4+dfsg1-7+b3 \
+    libxslt-dev \
+    qemu-kvm=1:3.1+dfsg-8+deb10u4 \
+    qemu-utils=1:3.1+dfsg-8+deb10u4 \
+    qemu=1:3.1+dfsg-8+deb10u4 \
+    rsync=3.1.3-6 \
+    ruby-dev=1:2.5.1 \
+    zlib1g-dev=1:1.2.11.dfsg-1
---
+++ scripts/linux/ci/install-vagrant.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# installation script for vagrant and vagrant-libvirt
+# to be run as root
+# tested on debian 10
+
+wget https://releases.hashicorp.com/vagrant/"${VAGRANT_VERSION}"/vagrant_"${VAGRANT_VERSION}"_x86_64.deb
+dpkg -i vagrant_"${VAGRANT_VERSION}"_x86_64.deb
+rm vagrant_"${VAGRANT_VERSION}"_x86_64.deb
+
+usermod -a -G libvirt vagrant
