076c56bb9ca4988f53e0ee6f5ca002f1f790ff7c
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-17 00:46:23 +0200

    Add suggested fixes

--- test/integration/default/inspec/kubernetes.rb
+++ test/integration/default/inspec/kubernetes.rb
@@ -37,6 +37,36 @@ control "kubernetes" do
     its('value') { should eq 1 }
   end
 
+  describe kernel_parameter('net.bridge.bridge-nf-call-ip6tables') do
+    its('value') { should eq 1 }
+  end
+
+  describe kernel_parameter('net.ipv4.ip_forward') do
+    its('value') { should eq 1 }
+  end
+
+  describe file("/etc/sysctl.conf") do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_readable.by_user('root') }
+    its('mode') { should cmp '0644' }
+    its('content') { should match /^net\.bridge\.bridge\-nf\-call\-iptables\s*=\s*1/ )
+    its('content') { should match /^net\.bridge\.bridge\-nf\-call\-ip6tables\s*=\s*1/ )
+    its('content') { should match /^net\.ipv4\.ip_forward\s*=\s*1/ )
+  end
+
+  describe file("/etc/fstab") do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_readable.by_user('root') }
+    its('mode') { should cmp '0644' }
+    its('content') { should match /^# \1/ )
+  end
+
   default_vars = YAML.load_file('ansible/roles/kubernetes/vars/main.yml')
   glusterfs_kernel_modules = default_vars['__ferrarimarco_kubernetes_glusterfs_kernel_modules']
 
