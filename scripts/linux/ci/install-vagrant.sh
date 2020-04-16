0034567c09cb68893c1ee64661ccd3bebe582adc
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-17 01:23:54 +0200

    Fix

--- scripts/linux/ci/install-vagrant.sh
+++ scripts/linux/ci/install-vagrant.sh
@@ -6,5 +6,3 @@
 wget https://releases.hashicorp.com/vagrant/"${VAGRANT_VERSION}"/vagrant_"${VAGRANT_VERSION}"_x86_64.deb
 dpkg -i vagrant_"${VAGRANT_VERSION}"_x86_64.deb
 rm vagrant_"${VAGRANT_VERSION}"_x86_64.deb
-
-usermod -a -G libvirt vagrant
