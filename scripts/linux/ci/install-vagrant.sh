b39a009a2ef1ca6cd834202cb31d13a0c5ea8207
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-17 11:32:22 +0200

    Install vagrant plugins in install-vagrant.sh

--- scripts/linux/ci/install-vagrant.sh
+++ scripts/linux/ci/install-vagrant.sh
@@ -6,3 +6,6 @@
 wget https://releases.hashicorp.com/vagrant/"${VAGRANT_VERSION}"/vagrant_"${VAGRANT_VERSION}"_x86_64.deb
 dpkg -i vagrant_"${VAGRANT_VERSION}"_x86_64.deb
 rm vagrant_"${VAGRANT_VERSION}"_x86_64.deb
+
+# Workaround for https://github.com/hashicorp/vagrant/issues/11524
+vagrant plugin install vagrant-hostsupdater
