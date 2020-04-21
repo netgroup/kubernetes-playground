cbb2d542a1cdff9511bffc0fadf4f2b9023ffdc0
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-22 00:12:41 +0200

    Run the Test-Kitchen suite on Travis (#185)

--- test/integration/default/inspec/docker.rb
+++ test/integration/default/inspec/docker.rb
@@ -6,11 +6,4 @@ control "docker" do
   describe command('which docker') do
     its('exit_status') { should eq 0 }
   end
-
-  config = YAML.load_file('ansible/roles/docker/defaults/main.yml')
-  docker_compose_version = config['docker_compose_version']
-
-  describe command('/usr/local/bin/docker-compose --version') do
-    its('stdout') { should match(docker_compose_version) }
-  end
 end
