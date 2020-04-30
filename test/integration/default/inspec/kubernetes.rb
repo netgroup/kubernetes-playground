2a6fb23447f26cfdd3da5f213166d4b2d07e693a
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-30 13:28:33 +0200

    Simplify sysctl test

--- test/integration/default/inspec/kubernetes.rb
+++ test/integration/default/inspec/kubernetes.rb
@@ -45,27 +45,36 @@ control "kubernetes" do
     its('value') { should eq 1 }
   end
 
-  sysctl_parse_options = {
-    assignment_regex: /^\s*([^=]*?)\s*=\s*(.*?)\s*$/
-  }
+    sysctl_parse_options = {
+        assignment_regex: /^\s*([^=]*?)\s*=\s*(.*?)\s*$/
+    }
 
-  describe parse_config_file('/etc/sysctl.conf',  sysctl_parse_options) do
-    its('net.bridge.bridge-nf-call-iptables') { should eq 1 }
-    its('net.bridge.bridge-nf-call-ip6tables') { should eq 1 }
-    its('net.ipv4.ip_forward') { should eq 1 }
-  end
+    kernel_parameters {
+        'net.bridge.bridge-nf-call-iptables': 1,
+        'net.bridge.bridge-nf-call-ip6tables': 1,
+        'net.ipv4.ip_forward': 1
+    }
 
-  describe file("/etc/sysctl.conf") do
-    it { should exist }
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    it { should be_readable.by_user('root') }
-    its('mode') { should cmp '0644' }
-    its('content') { should match /^net\.bridge\.bridge\-nf\-call\-iptables\s*=\s*1/ }
-    its('content') { should match /^net\.bridge\.bridge\-nf\-call\-ip6tables\s*=\s*1/ }
-    its('content') { should match /^net\.ipv4\.ip_forward\s*=\s*1/ }
-  end
+    sysctl_configuration_file_path = '/etc/sysctl.conf'
+
+    kernel_parameters.each do |key,value|
+        describe kernel_parameter(key) do
+            its('value') { should eq value }
+        end
+
+        describe parse_config_file(sysctl_configuration_file_path, sysctl_parse_options).params[key] do
+            it { should eq "#{value}" }
+        end
+    end
+
+    describe file(sysctl_configuration_file_path) do
+        it { should exist }
+        it { should be_file }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_readable.by_user('root') }
+        its('mode') { should cmp '0644' }
+    end
 
     describe etc_fstab.where { file_system_type.match(/swap/) } do
         it { should_not be_configured }
