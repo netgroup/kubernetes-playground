b06c69fa4aeb16a57ea8d4b820d82570b656584b
Marco Ferrari <ferrari.marco@gmail.com>
2020-04-30 15:50:04 +0200

    Fix init

--- test/integration/default/inspec/kubernetes.rb
+++ test/integration/default/inspec/kubernetes.rb
@@ -49,7 +49,7 @@ control "kubernetes" do
         assignment_regex: /^\s*([^=]*?)\s*=\s*(.*?)\s*$/
     }
 
-    kernel_parameters {
+    kernel_parameters = {
         'net.bridge.bridge-nf-call-iptables' => 1,
         'net.bridge.bridge-nf-call-ip6tables' => 1,
         'net.ipv4.ip_forward' => 1
