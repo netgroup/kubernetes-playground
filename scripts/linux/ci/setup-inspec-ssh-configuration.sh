433aa7c23bde825db87a036b4be0faaccd5603e8
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-04 17:31:49 +0200

    ssh config script

---
+++ scripts/linux/ci/setup-inspec-ssh-configuration.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+SSH_HOST="$1"
+
+echo "Setting environment variables for InSpec to connect to $SSH_HOST via ssh..."
+
+SSH_CONFIG_FILE_PATH="$HOME"/.ssh/config
+echo "Current ssh configuration ($SSH_CONFIG_FILE_PATH) $(cat "$SSH_CONFIG_FILE_PATH")"
+
+SSH_CONFIG_FOR_HOST="$(ssh -G "$SSH_HOST")"
+echo "ssh configuration for $SSH_HOST: $SSH_CONFIG_FOR_HOST"
+
+INSPEC_SSH_USER=
+INSPEC_SSH_HOST=
+INSPEC_SSH_PRIVATE_KEY_PATH=
+INSPEC_SSH_PORT=
+
+export INSPEC_SSH_USER
+export INSPEC_SSH_HOST
+export INSPEC_SSH_PRIVATE_KEY_PATH
+export INSPEC_SSH_PORT
+
+echo "Configuration variables for $SSH_HOST: INSPEC_SSH_USER=$INSPEC_SSH_USER, INSPEC_SSH_HOST=$INSPEC_SSH_HOST, INSPEC_SSH_PRIVATE_KEY_PATH=$INSPEC_SSH_PRIVATE_KEY_PATH, INSPEC_SSH_PORT=$INSPEC_SSH_PORT"
+
+unset SSH_HOST
+unset SSH_CONFIG_FILE_PATH
+
+set +e
