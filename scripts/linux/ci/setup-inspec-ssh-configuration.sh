976059a02715b1f862fc09f6f0c6aac7993fca24
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-04 22:20:58 +0200

    Fix config

--- scripts/linux/ci/setup-inspec-ssh-configuration.sh
+++ scripts/linux/ci/setup-inspec-ssh-configuration.sh
@@ -6,16 +6,13 @@ SSH_HOST="$1"
 
 echo "Setting environment variables for InSpec to connect to $SSH_HOST via ssh..."
 
-SSH_CONFIG_FILE_PATH="$HOME"/.ssh/config
-echo "Current ssh configuration ($SSH_CONFIG_FILE_PATH) $(cat "$SSH_CONFIG_FILE_PATH")"
-
-SSH_CONFIG_FOR_HOST="$(vagrant ssh-config "$SSH_HOST")"
+SSH_CONFIG_FOR_HOST="$(VAGRANT_SUPPRESS_OUTPUT="true" vagrant ssh-config "$SSH_HOST")"
 echo "vagrant ssh configuration for $SSH_HOST: $SSH_CONFIG_FOR_HOST"
 
-INSPEC_SSH_USER="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=User ).*)')"
-INSPEC_SSH_HOST="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=HostName ).*)')"
-INSPEC_SSH_PRIVATE_KEY_PATH="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=IdentityFile ).*)')"
-INSPEC_SSH_PORT="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=Port ).*)')"
+INSPEC_SSH_USER="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=User ).*')"
+INSPEC_SSH_HOST="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=HostName ).*')"
+INSPEC_SSH_PRIVATE_KEY_PATH="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=IdentityFile ).*')"
+INSPEC_SSH_PORT="$(echo "$SSH_CONFIG_FOR_HOST" | grep -m1 -oP '(?<=Port ).*')"
 
 echo "Configuration variables for $SSH_HOST: INSPEC_SSH_USER=$INSPEC_SSH_USER, INSPEC_SSH_HOST=$INSPEC_SSH_HOST, INSPEC_SSH_PRIVATE_KEY_PATH=$INSPEC_SSH_PRIVATE_KEY_PATH, INSPEC_SSH_PORT=$INSPEC_SSH_PORT"
 
@@ -25,6 +22,5 @@ export INSPEC_SSH_PRIVATE_KEY_PATH
 export INSPEC_SSH_PORT
 
 unset SSH_HOST
-unset SSH_CONFIG_FILE_PATH
 
 set +e
