f6c1f5cec6ffe65df7159de59de896aa4d67338d
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-22 00:47:05 +0200

    Extend diagnostics

--- scripts/linux/ci/diagnostics.sh
+++ scripts/linux/ci/diagnostics.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+echo "Current user: $(whoami)"
+echo "Current working directory: $(pwd)"
+
+if command -v git >/dev/null 2>&1; then
+    echo "git status: $(git status)"
+    echo "git branch: $(git branch)"
+    echo "git log: $(git log --oneline --graph --all)"
+fi
+
+echo
+
+[ -f env.yaml ] && echo "env.yaml contents: $(cat env.yaml)"
+
 command -v dpkg >/dev/null && echo "Docker version: $(docker --version)"
 
 command -v tree >/dev/null && echo "Current directory tree: $(tree .)"
@@ -28,3 +41,18 @@ if command -v gem >/dev/null 2>&1; then
 fi
 
 command -v bundle >/dev/null && echo "bundle list: $(bundle list)"
+
+if command -v inspec >/dev/null 2>&1; then
+    echo "inspec help: $(inspec -h)"
+    echo "inspec check help: $(inspec check -h)"
+fi
+
+[ -f /var/log/libvirt/libvirtd.log ] && echo "libvirtd.log contents: $(cat /var/log/libvirt/libvirtd.log)"
+[ -f "$HOME"/.virt-manager/virt-manager.log ] && echo "virt-manager.log contents: $(cat "$HOME"/.virt-manager/virt-manager.log)"
+
+ls -al /var/log/libvirt/qemu/
+command -v virsh >/dev/null && echo "virsh list: $(virsh list)"
+
+[ -f /var/log/syslog ] && echo "syslog contents: $(cat /var/log/syslog)"
+
+command -v journalctl >/dev/null && echo "journalctl: $(journalctl --no-pager)"
