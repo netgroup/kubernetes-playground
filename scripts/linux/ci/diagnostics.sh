17f6f8d8cc039fa496885aac26198bf7d5973369
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-22 20:53:10 +0200

    Two steps

--- scripts/linux/ci/diagnostics.sh
+++ scripts/linux/ci/diagnostics.sh
@@ -3,17 +3,15 @@
 echo "Current user: $(whoami)"
 echo "Current working directory: $(pwd)"
 
+command -v docker >/dev/null && echo "Docker version: $(docker --version)"
+
 if command -v git >/dev/null 2>&1; then
     echo "git status: $(git status)"
     echo "git branch: $(git branch)"
-    echo "git log: $(git log --oneline --graph --all)"
+    echo "git log: $(git log --oneline --graph --all | tail -n 10)"
 fi
 
 [ -f env.yaml ] && echo "env.yaml contents: $(cat env.yaml)"
-
-command -v dpkg >/dev/null && echo "Docker version: $(docker --version)"
-
-command -v tree >/dev/null && echo "Current directory tree: $(tree .)"
 
 [ -f /etc/exports ] && echo "/etc/exports contents: $(cat /etc/exports)"
 
@@ -23,7 +21,6 @@ echo "etc hosts content: $(cat /etc/hosts)"
 echo "environment: $(env | sort)"
 echo "lsmod: $(lsmod | sort)"
 
-command -v dpkg >/dev/null && echo "Installed debian packages: $(dpkg -l | sort)"
 command -v kvm-ok >/dev/null && echo "kvm-ok: $(kvm-ok)"
 
 if command -v vagrant >/dev/null 2>&1; then
@@ -32,12 +29,19 @@ if command -v vagrant >/dev/null 2>&1; then
     echo "vagrant ssh-config: $(VAGRANT_SUPPRESS_OUTPUT="true" vagrant ssh-config)"
 fi
 
+command -v bundle >/dev/null && echo "bundle list: $(bundle list)"
+
+echo "/var/log/libvirt/qemu contents: $(ls -al /var/log/libvirt/qemu/)"
+command -v virsh >/dev/null && echo "virsh list: $(virsh list)"
+
+# More verbose stuff
+
+command -v tree >/dev/null && echo "Current directory tree: $(tree .)"
+
 if command -v gem >/dev/null 2>&1; then
     echo "gem environment: $(gem environment)"
     echo "Locally installed gems: $(gem query --local)"
 fi
-
-command -v bundle >/dev/null && echo "bundle list: $(bundle list)"
 
 if command -v inspec >/dev/null 2>&1; then
     echo "inspec help: $(inspec -h)"
@@ -47,9 +51,6 @@ fi
 [ -f /var/log/libvirt/libvirtd.log ] && echo "libvirtd.log contents: $(cat /var/log/libvirt/libvirtd.log)"
 [ -f "$HOME"/.virt-manager/virt-manager.log ] && echo "virt-manager.log contents: $(cat "$HOME"/.virt-manager/virt-manager.log)"
 
-ls -al /var/log/libvirt/qemu/
-command -v virsh >/dev/null && echo "virsh list: $(virsh list)"
-
+command -v dpkg >/dev/null && echo "Installed debian packages: $(dpkg -l | sort)"
 [ -f /var/log/syslog ] && echo "syslog contents: $(cat /var/log/syslog)"
-
 command -v journalctl >/dev/null && echo "journalctl: $(journalctl --no-pager)"
