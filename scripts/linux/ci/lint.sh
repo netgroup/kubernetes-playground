16b4a557dc6094f99e9d60d39c7f0e39fa82ff3e
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-24 00:48:28 +0200

    update markdownlint

--- scripts/linux/ci/lint.sh
+++ scripts/linux/ci/lint.sh
@@ -3,11 +3,6 @@
 PWD="$(pwd)"
 echo "Current working directory: $PWD"
 
-NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-# shellcheck source=/dev/null
-[ -s "$NVM_DIR"/nvm.sh ] && . "$NVM_DIR"/nvm.sh
-nvm use
-
 git diff-tree --check "$(git hash-object -t tree /dev/null)" HEAD
 
 find "$(pwd)" -type f -not -path "*/\.git/*" -not -path "*/\node_modules/*" >tmp
