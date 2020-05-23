e3d3c746367c88fb44ac7884f2ea17318f3df303
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-23 12:00:39 +0200

    fix gimme

--- scripts/linux/ci/install-linting-tools.sh
+++ scripts/linux/ci/install-linting-tools.sh
@@ -15,11 +15,12 @@ pip install -r requirements.txt
 echo "Gimme version: $(gimme --version)"
 echo "Gimme help: $(gimme --help)"
 echo "Installable go versions: $(gimme --known)"
+
 GIMME_GO_VERSION="1.14.x"
-#GIMME_OS=linux
-#GIMME_ARCH=amd64
-echo "Installing Go $GIMME_GO_VERSION"
-gimme "$GO_VERSION"
+GIMME_ARCH=amd64
+GIMME_OS=linux
+echo "Installing Go $GIMME_GO_VERSION ($GIMME_ARCH $GIMME_OS)"
+GIMME_GO_VERSION GIMME_ARCH GIMME_OS gimme
 
 echo "Go version: $(go --version)"
 
