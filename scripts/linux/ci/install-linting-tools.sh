fb8ee0be783c846cf2ec329a2a07da072f16ab56
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-23 11:54:42 +0200

    Gimme

--- scripts/linux/ci/install-linting-tools.sh
+++ scripts/linux/ci/install-linting-tools.sh
@@ -12,6 +12,20 @@
 
 pip install -r requirements.txt
 
+echo "Gimme version: $(gimme --version)"
+echo "Gimme help: $(gimme --help)"
+echo "Installable go versions: $(gimme --known)"
+GIMME_GO_VERSION="1.14.x"
+#GIMME_OS=linux
+#GIMME_ARCH=amd64
+echo "Installing Go $GIMME_GO_VERSION"
+gimme "$GO_VERSION"
+
+echo "Go version: $(go --version)"
+
+# shellcheck source=/dev/null
+. "$HOME"/.gimme/envs/go1.4.env
+
 GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
 
 npm install
