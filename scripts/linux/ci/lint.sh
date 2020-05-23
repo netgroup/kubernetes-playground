4c36e3674559709ca4c87c3554a1fc4ab1bcdb5d
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-23 23:22:23 +0200

    exclude node modules

--- scripts/linux/ci/lint.sh
+++ scripts/linux/ci/lint.sh
@@ -5,7 +5,7 @@ echo "Current working directory: $PWD"
 
 git diff-tree --check "$(git hash-object -t tree /dev/null)" HEAD
 
-find "$(pwd)" -type f -not -path "*/\.git/*" >tmp
+find "$(pwd)" -type f -not -path "*/\.git/*" -not -path "*/\node_modules/*" >tmp
 while IFS= read -r file; do
     case "$(git diff --no-index --numstat /dev/null "$file")" in
     "$(printf '%s\t-\t' -)"*)
@@ -13,8 +13,8 @@ while IFS= read -r file; do
         continue
         ;;
     *)
-        echo "Checking $file"
-        [ -z "$(tail -c1 "$file")" ] || exit 1
+        echo "Checking if $file ends with a newline"
+        [ -z "$(tail -c1 "$file")" ] || echo "ERROR: $file doesn't end with a newline" exit 1
         ;;
     esac
 done <tmp
@@ -34,7 +34,7 @@ while IFS= read -r -d '' file; do
     echo "Linting $f"
     if [ ! -x "$f" ]; then echo "Error: $f is not executable!"; fi
     docker run -v "$(pwd)":/mnt:ro --rm -t koalaman/shellcheck:v0.7.1 "$f"
-done < <(find "$(pwd)" -type f -not -path "*/\.git/*" -not -name "*.md" -exec grep -Eq '^#!(.*/|.*env +)(sh|bash|ksh)' {} \; -print0)
+done < <(find "$(pwd)" -type f -not -path "*/\.git/*" -not -name "*.md" -not -path "*/\node_modules/*" -exec grep -Eq '^#!(.*/|.*env +)(sh|bash|ksh)' {} \; -print0)
 
 yamllint --strict "$(git ls-files '*.yaml' '*.yml')"
 
