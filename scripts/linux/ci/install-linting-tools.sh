e0225f2ba3ad38ae4334312843858a77c4da6d8c
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-23 18:01:15 +0200

    python

--- scripts/linux/ci/install-linting-tools.sh
+++ scripts/linux/ci/install-linting-tools.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+set -o pipefail
 
 # - name: "YAMLlint"
 # language: python
@@ -6,23 +9,24 @@
 # - name: "Markdownlint"
 # language: node_js
 # node_js: node
-# - name: "shfmt"
-# language: go
-# go: "1.14.1"
+
+echo "Python path: $(command -v python)"
+echo "Python version: $(python --version)"
+
+echo "Python 3 path: $(command -v python3)"
+echo "Python 3 version: $(python3 --version)"
+echo "pip 3 path: $(command -v pip3)"
+echo "pip 3 version: $(pip3 --version)"
 
 pip3 install -r requirements.txt
 
 echo "Gimme version: $(gimme --version)"
-echo "Gimme help: $(gimme --help)"
-echo "Installable go versions: $(gimme --known)"
-
-GIMME_GO_VERSION="1.14.x"
+GIMME_GO_VERSION="1.14.3"
 GIMME_ARCH=amd64
 GIMME_OS=linux
 echo "Installing Go $GIMME_GO_VERSION ($GIMME_ARCH $GIMME_OS)"
-eval "$(GIMME_GO_VERSION=$GIMME_GO_VERSION GIMME_ARCH=$GIMME_ARCH=amd64 GIMME_OS=$GIMME_OS gimme)"
-
-echo "Go version: $(go --version)"
+eval "$(GIMME_GO_VERSION=$GIMME_GO_VERSION GIMME_ARCH=$GIMME_ARCH GIMME_OS=$GIMME_OS gimme)"
+echo "Go version: $(go version)"
 
 GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
 
