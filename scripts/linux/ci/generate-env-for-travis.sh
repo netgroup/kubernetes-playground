0e8a59164435460a3b7a49d5e9f0e32739511af1
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-25 12:05:56 +0200

    Verbose

--- scripts/linux/ci/generate-env-for-travis.sh
+++ scripts/linux/ci/generate-env-for-travis.sh
@@ -4,6 +4,7 @@ echo "Generating an env.yaml for the Travis CI environment..."
 
 cat >env.yaml <<EOF
 conf:
+  additional_ansible_arguments: "-vvv"
   playground_name: k8s-play
   vagrant_provider: libvirt
 EOF
