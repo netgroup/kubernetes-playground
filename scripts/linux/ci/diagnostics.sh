a91d0c17a6a53a7c41dbd85d896a16b0a8e08164
Marco Ferrari <ferrari.marco@gmail.com>
2020-05-26 00:48:50 +0200

    verbose

--- scripts/linux/ci/diagnostics.sh
+++ scripts/linux/ci/diagnostics.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-if ! TEMP="$(getopt -o n --long vagrant-vm-name: \
+if ! TEMP="$(getopt -o n:v --long vagrant-vm-name:,verbose \
     -n 'diagnostics' -- "$@")"; then
     echo "Terminating..." >&2
     exit 1
@@ -11,12 +11,18 @@ fi
 eval set -- "$TEMP"
 
 vagrant_vm_name=
+verbose=
 
 while true; do
     case "$1" in
-    -a | --vagrant-vm-name)
+    -n | --vagrant-vm-name)
         vagrant_vm_name="$2"
         shift 2
+        ;;
+    -v | --verbose)
+        verbose=enabled
+        shift
+        break
         ;;
     --)
         shift
@@ -82,7 +88,6 @@ if command -v vagrant >/dev/null 2>&1; then
 
     if [ -z "$vagrant_vm_name" ]; then
         echo "vagrant ssh-config: $(VAGRANT_SUPPRESS_OUTPUT="true" vagrant ssh-config "$vagrant_vm_name")"
-        echo "vagrant box diagnostics: $(VAGRANT_SUPPRESS_OUTPUT="true" vagrant ssh "$vagrant_vm_name" -C "/vagrant/scripts/linux/ci/diagnostics.sh")"
     fi
 fi
 
@@ -91,13 +96,8 @@ command -v bundle >/dev/null && echo "bundle list: $(bundle list)"
 echo "/var/log/libvirt/qemu contents: $(ls -al /var/log/libvirt/qemu/)"
 command -v virsh >/dev/null && echo "virsh list: $(virsh list)"
 
-# More verbose stuff
-
-command -v tree >/dev/null && echo "Current directory tree: $(tree .)"
-
 if command -v gem >/dev/null 2>&1; then
     echo "gem environment: $(gem environment)"
-    echo "Locally installed gems: $(gem query --local)"
 fi
 
 if [ -s "$NVM_DIR"/nvm.sh ]; then
@@ -113,15 +113,40 @@ fi
 if command -v npm >/dev/null 2>&1; then
     echo "npm command: $(command -v npm)"
     echo "npm version: $(npm --version)"
-    npm list -g --depth=0
 fi
 
-if command -v inspec >/dev/null 2>&1; then
-    echo "inspec help: $(inspec -h)"
-    echo "inspec check help: $(inspec check -h)"
+if [ "$verbose" = "enabled" ]; then
+    if command -v docker >/dev/null 2>&1; then
+        echo "-------- Docker --------"
+        echo "Docker info (JSON): $(docker info --format '{{json .}}')"
+        echo "----------------"
+    fi
+    if command -v vagrant >/dev/null 2>&1; then
+        if [ -z "$vagrant_vm_name" ]; then
+            echo "vagrant box diagnostics: $(VAGRANT_SUPPRESS_OUTPUT="true" vagrant ssh "$vagrant_vm_name" -C "/vagrant/scripts/linux/ci/diagnostics.sh")"
+        fi
+    fi
+
+    command -v tree >/dev/null && echo "Current directory tree: $(tree .)"
+
+    if command -v gem >/dev/null 2>&1; then
+        echo "Locally installed gems: $(gem query --local)"
+    fi
+
+    if command -v inspec >/dev/null 2>&1; then
+        echo "inspec help: $(inspec -h)"
+        echo "inspec check help: $(inspec check -h)"
+    fi
+
+    if command -v npm >/dev/null 2>&1; then
+        echo "Installed npm packages: $(npm list -g --depth=0)"
+    fi
+
+    [ -f /var/log/libvirt/libvirtd.log ] && echo "libvirtd.log contents: $(cat /var/log/libvirt/libvirtd.log)"
+    [ -f "$HOME"/.virt-manager/virt-manager.log ] && echo "virt-manager.log contents: $(cat "$HOME"/.virt-manager/virt-manager.log)"
+
+    command -v dpkg >/dev/null && echo "Installed debian packages: $(dpkg -l | sort)"
+
+    [ -f /var/log/syslog ] && echo "syslog contents: $(cat /var/log/syslog)"
+    command -v journalctl >/dev/null && echo "journalctl: $(journalctl --no-pager)"
 fi
-
-[ -f /var/log/libvirt/libvirtd.log ] && echo "libvirtd.log contents: $(cat /var/log/libvirt/libvirtd.log)"
-[ -f "$HOME"/.virt-manager/virt-manager.log ] && echo "virt-manager.log contents: $(cat "$HOME"/.virt-manager/virt-manager.log)"
-
-command -v dpkg >/dev/null && echo "Installed debian packages: $(dpkg -l | sort)"
