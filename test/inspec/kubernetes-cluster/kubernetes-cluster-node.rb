58b11241201501cbdbae485a8650e40ed52f55d0
Marco Ferrari <ferrari.marco@gmail.com>
2020-09-13 18:46:24 +0200

    Fix linting errors

--- test/inspec/kubernetes-cluster/kubernetes-cluster-node.rb
+++ test/inspec/kubernetes-cluster/kubernetes-cluster-node.rb
@@ -1,24 +1,24 @@
-require 'yaml'
+require "yaml"
 
 control "kubernetes-cluster-node" do
     title "kubernetes cluster node check"
     desc "This control checks that the the target is a valid Kubernetes cluster node"
 
-    describe service('kubelet') do
+    describe service("kubelet") do
         it { should be_installed }
         it { should be_enabled }
     end
 
-    all_group_vars = YAML.load_file('ansible/group_vars/all.yaml')
-    ip_to_host_mappings = all_group_vars['ip_to_host_mappings']
+    all_group_vars = YAML.load_file("ansible/group_vars/all.yaml")
+    ip_to_host_mappings = all_group_vars["ip_to_host_mappings"]
 
     ip_to_host_mappings.each { |ip_to_host_mapping|
-        ip_v4_address = ip_to_host_mapping['ip_v4_address']
-        hostname = ip_to_host_mapping['hostname']
+        ip_v4_address = ip_to_host_mapping["ip_v4_address"]
+        hostname = ip_to_host_mapping["hostname"]
 
         describe host(hostname) do
             it { should be_resolvable }
-            its('ipaddress') { should include ip_v4_address }
+            its("ipaddress") { should include ip_v4_address }
         end
     }
 end
