c982f3040434d6f1f8c97b69421f8d8f06480e8e
Marco Ferrari <ferrari.marco@gmail.com>
2020-09-25 21:35:35 +0200

    Upgrade super-linter to 3.11.0 and fix linting errors (#277)

--- scripts/linux/ci/lint.sh
+++ scripts/linux/ci/lint.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-docker run -t \
-    -v "$(pwd)":/kubernetes-playground:ro \
-    garethr/kubeval:0.15.0 \
-    --strict -d /kubernetes-playground/kubernetes || exit 1
-
 docker run --rm -it \
-    -v "$(pwd)":/workspace \
+    -v "$(pwd)":/workspace:ro \
     -w="/workspace" \
     -e ACTIONS_RUNNER_DEBUG=true \
-    -e ANSIBLE_DIRECTORY=./ansible \
     -e DEFAULT_WORKSPACE=/workspace \
     -e DISABLE_ERRORS=false \
+    -e ERROR_ON_MISSING_EXEC_BIT=true \
     -e LINTER_RULES_PATH=. \
     -e MULTI_STATUS=false \
     -e RUN_LOCAL=true \
     -e VALIDATE_ALL_CODEBASE=true \
-    ghcr.io/github/super-linter:v3.10.0 || exit 1
+    ghcr.io/github/super-linter:v3.11.0 || exit 1
 
 inspec check --chef-license=accept test/inspec/kubernetes-playground
