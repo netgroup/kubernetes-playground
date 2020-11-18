e6a10a7c4def3441dd2c4fb4a84a1ac4e4fea652
Francesco Lombardo <franclombardo@gmail.com>
2020-11-18 17:49:02 +0100

    modified ipv6 configuration

--- scripts/linux/configure-ipv6-interface.sh
+++ scripts/linux/configure-ipv6-interface.sh
@@ -5,15 +5,20 @@ echo "Configuring the IPv6 interface"
 IPV6_ADDRESS="$1"
 IPV6_NETMASK="$2"
 DEVICE="$3"
-UUID="$4"
+NET_CONFIG_PATH="/etc/network/interfaces"
 
-echo "Current NetworkManager UUID $UUID assigned to $DEVICE"
 
 echo "Adding $IPV6_ADDRESS to $DEVICE"
-nmcli con mod "$UUID" ipv6.address "$IPV6_ADDRESS/$IPV6_NETMASK" ipv6.method manual
 
-echo "Restarting NetworkManager"
-systemctl restart network
+{
+    echo "auto $DEVICE"
+    echo "iface $DEVICE inet6 static"
+    echo "  address $IPV6_ADDRESS"
+    echo "  netmask $IPV6_NETMASK"
+} >> "$NET_CONFIG_PATH"
+
+echo "Restarting Network"
+systemctl restart networking
 
 echo "Getting all the IP addresses: $(ip -o addr show scope global)"
 
@@ -21,4 +26,3 @@ modprobe -- ip_vs
 modprobe -- ip_vs_rr
 modprobe -- ip_vs_wrr
 modprobe -- ip_vs_sh
-
