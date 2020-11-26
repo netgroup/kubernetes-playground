19ec46d1a1ac8345a7c5ee6eded9a13dae61ae68
Marco Ferrari <ferrari.marco@gmail.com>
2020-11-26 22:11:02 +0100

    Use a single step

--- scripts/linux/ci/lint.sh
+++ scripts/linux/ci/lint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 docker run --rm -it \
-    -v "$(pwd)":/workspace:ro \
+    -v "$(pwd)":/workspace \
     -w="/workspace" \
-    -e ACTIONS_RUNNER_DEBUG=true \
     -e DEFAULT_WORKSPACE=/workspace \
     -e DISABLE_ERRORS=false \
     -e ERROR_ON_MISSING_EXEC_BIT=true \
@@ -11,6 +10,6 @@ docker run --rm -it \
     -e MULTI_STATUS=false \
     -e RUN_LOCAL=true \
     -e VALIDATE_ALL_CODEBASE=true \
-    ghcr.io/github/super-linter:v3.11.0 || exit 1
+    ghcr.io/github/super-linter:v3.15.3 || exit 1
 
 inspec check --chef-license=accept test/inspec/kubernetes-playground
