ea63f8280b884fdcdbd20a9e507dcdee8e44699a
Marco Ferrari <ferrari.marco@gmail.com>
2022-05-12 13:11:13 +0000

    Install Vagrant 2.2.19

--- scripts/linux/ci/install-vagrant.sh
+++ scripts/linux/ci/install-vagrant.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+VAGRANT_VERSION="${VAGRANT_VERSION:-"2.2.19"}"
+
 wget https://releases.hashicorp.com/vagrant/"${VAGRANT_VERSION}"/vagrant_"${VAGRANT_VERSION}"_x86_64.deb
 dpkg -i vagrant_"${VAGRANT_VERSION}"_x86_64.deb
 rm vagrant_"${VAGRANT_VERSION}"_x86_64.deb
